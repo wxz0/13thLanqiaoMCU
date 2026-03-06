6fdf7cb436d219a241a7238d0aa16c4c0876f2f3
cxzwn_PRIME <77762845@qq.com>
2026-03-06 17:37:01 +0800

    create

---
+++ 省赛/hardware/key.c
@@ -0,0 +1,9 @@
+#include "key.h"
+
+uint8_t key_getstate(void)
+{
+ uint8_t state = 0;
+ 	
+	
+
+}
---
+++ 省赛/main.c
@@ -0,0 +1,14 @@
+#include <STC15F2K60S2.H>
+
+
+void main(void)
+{
+	
+	while(1)
+	{
+	
+		
+	
+	}
+	
+}
